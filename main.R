de0206743f755346da7cae6e3b537c0aca528fdd
Kevin J. Pedde <gammalight@users.noreply.github.com>
2018-10-08 16:08:54 -0500

    Add files via upload
    making initial commit of the file I will use to predict customer churn from the Kaggle dataset

---
+++ main.R
@@ -0,0 +1,10 @@
+#this script is used to predict customer churn 
+#the data was downloaded off of Kaggle
+
+#load packages that I will need to
+#read in data
+#conduct exploratory analysis
+#manipulate data
+#build several models
+
+library(dplyr)
